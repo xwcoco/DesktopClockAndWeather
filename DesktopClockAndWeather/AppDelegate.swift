7760ae761ca1690cb413283fe13a5e47ecfce736
徐卫 <xuwei@dfsoft.com.cn>
2018-10-18 14:55:36 +0800

    Initial Commit

---
+++ DesktopClockAndWeather/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DesktopClockAndWeather
+//
+//  Created by xuwei on 2018/10/18.
+//  Copyright © 2018年 xuwei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DesktopClockAndWeather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DesktopClockAndWeather
+//
+//  Created by xuwei on 2018/10/18.
+//  Copyright © 2018年 xuwei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
