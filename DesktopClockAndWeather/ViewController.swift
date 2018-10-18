279222f8e4c7a08c764abdb3cf2d025e7c9c3d89
xwcoco <xwcoco@msn.com>
2018-10-18 21:31:48 +0800

    add images

--- DesktopClockAndWeather/ViewController.swift
+++ DesktopClockAndWeather/ViewController.swift
@@ -13,10 +13,15 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var weekLabel: NSTextField!
     @IBOutlet weak var dateLabel: NSTextField!
+    @IBOutlet weak var timeLabel: NSTextField!
+    @IBOutlet weak var dotImage: NSImageView!
+    var timer: Timer? = nil
+    var showDot: Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
         let currentDate = Date()
 
         //let calendar = Calendar.current
@@ -37,7 +42,32 @@ class ViewController: NSViewController {
 //        let day = todayDate.day
         self.dateLabel.stringValue = String(day)
         
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "HH mm"
+//        self.timeLabel.stringValue = dateFormatter.string(from: currentDate)
         
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(timerAction),
+                                     userInfo: nil,
+                                     repeats: true)
+        timerAction()
+       
+
+    }
+    
+    @objc dynamic func timerAction() {
+        if self.showDot {
+            self.dotImage.isHidden = true
+            self.showDot = false
+        } else {
+            self.dotImage.isHidden = false
+            self.showDot = true
+        }
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH mm"
+        self.timeLabel.stringValue = dateFormatter.string(from: currentDate)
     }
 
     override var representedObject: Any? {
